cf75da3d4e231d8aa1eddc35e091424615735908
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-11-10 19:42:10 +0300

    solved the task

--- main.dart
+++ main.dart
@@ -1,4 +1,32 @@
-void main(){
-  
-  
+void main() {
+  double tempInFahrenheit = 86;
+  double fToCelsius = (tempInFahrenheit - 32) / 1.8;
+  print(fToCelsius);
+/////--------
+  double cToFahrenheit = fToCelsius * 1.8 + 32.00;
+  print(cToFahrenheit);
+/////--------
+
+  int mark = 55;
+
+  if (mark >= 80) {
+    print("A");
+  } else if (mark >= 70) {
+    print("B");
+  } else if (mark >= 60) {
+    print("C");
+  } else if (mark >= 50) {
+    print("D");
+  } else
+    (print("F"));
+/////--------
+  int i = 6;
+  if (i % 3 == 0 && i % 5 == 0) {
+    print("fizz buzz");
+  } else if (i % 3 == 0) {
+    print("fizz");
+  } else if (i % 5 == 0) {
+    print("buzz");
+  } else
+    (print(i));
 }
